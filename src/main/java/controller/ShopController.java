4b61150fa407c3ebeff39beb84bfab1836506183
GijsArkesteijn <gijsarkesteijn@gmail.com>
2021-05-22 15:06:45 +0200

    shop page created

---
+++ src/main/java/controller/ShopController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class ShopController {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    public void backToHome(MouseEvent mouseEvent) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/HomeScreen.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+}
