2f1a3b7380b563095b265e70080382201809767c
GijsArkesteijn <gijsarkesteijn@gmail.com>
2021-06-03 16:27:37 +0200

    receipt werkt
    Receipt werkt maar heeft gehardcode User en producten. Design aanpassing nodig

--- src/main/java/Receipt/Receipt.java
+++ src/main/java/Receipt/Receipt.java
@@ -1,35 +1,54 @@
 package Receipt;
 
 import Account.Account;
+import Account.User;
 import Login.login;
 import Webshop.Product;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Receipt {
-    private Account buyer;
+    private User buyer;
     private ArrayList<Product> products;
     private int code;
 
-    public Receipt(){
+    public Receipt(User buyer, ArrayList<Product> products){
+        this.buyer=buyer;
+        this.products=new ArrayList<>();
+        this.products.addAll(products);
 
     }
     public String getReceipt(){
-        String bon=null;
-        int totaalPrijsExclKorting=0;
+        String bon="";
+        double totaalPrijsExclKorting=0;
         bon= bon+buyer.getName()+"\n";
         for(Product product :products){
-//          totaalPrijs=totaalPrijs product.getPrice();
-            bon+= product.getName()+ "de prijs"+"\n";
+            totaalPrijsExclKorting=totaalPrijsExclKorting+ product.getPrice();
+            bon+= product.getName()+ " "+product.getPrice()+"\n";
         }
-//        int kortingPercentage= buyer.getBatch().getKorting();
-//        int korting=kortingPercentage*totaalPrijs/100
-//        int totaalPrijsInclKorting =totaalPrijs-korting;
 
-//        bon+= buyer.getBatch().getKorting();
-//        bon+= korting;
-//        bon+= totaalPrijsInclKorting;
+        double korting=getKortingsPercentage()*totaalPrijsExclKorting;
+        double totaalPrijsInclKorting =totaalPrijsExclKorting-korting;
+
+        bon+= getKortingsPercentage()+"% \n";
+        bon+= korting+"\n";
+        bon+= totaalPrijsInclKorting+"\n";
         return bon;
     }
+    public double getKortingsPercentage(){
 
+        String rank = buyer.getRank();
+        double kortingPercentage= 0;
+        if(rank.equals("Gold")){
+            kortingPercentage = 0.15;
+        }
+        else if(rank.equals("Silver")){
+            kortingPercentage = 0.10;
+        }
+        else if(rank.equals("Bronze")){
+            kortingPercentage = 0.05;
+        }
+        return kortingPercentage;
+    }
 }
--- src/main/java/controller/ShoppingCartController.java
+++ src/main/java/controller/ShoppingCartController.java
@@ -1,18 +1,58 @@
 package controller;
 
+import Account.User;
+import Database.Database;
+import Login.login;
+import Receipt.Receipt;
+import Webshop.Category;
+import Webshop.Product;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import org.w3c.dom.Text;
 
+import javafx.scene.control.TextField;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ShoppingCartController {
     @FXML
     private AnchorPane rootPane;
-
+    @FXML
+    TextField receiptTextField;
+    @FXML
+    TextArea bruh;
+    public void initialize(){
+        System.out.println("Gamign");
+        ArrayList<Product> products = new ArrayList<>();
+        ArrayList<String> beroepen = new ArrayList<>();
+        beroepen.add("aa");
+        beroepen.add("bb");
+        beroepen.add("cc");
+        User ingelogdeUser = new User("gert","gertje","gertje@gmail.com","gert","m","25-09-2001","admin",beroepen);
+        products.add(new Product("product1",2.10,new Category("gaming")));
+        products.add(new Product("product2",2.10,new Category("gaming")));
+        Receipt receipt = new Receipt(ingelogdeUser,products);
+        System.out.println(receipt.getReceipt());
+//        receiptTextField.setText(receipt.getReceipt());
+        bruh.setText(receipt.getReceipt());
+    }
     public void backToShop(MouseEvent mouseEvent) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/ShopScreen.fxml"));
         rootPane.getChildren().setAll(pane);
+    }
+    public void printReceipt(MouseEvent mouseEvent) throws IOException{
+        ArrayList<Product> products = new ArrayList<>();
+        ArrayList<String> beroepen = new ArrayList<>();
+        beroepen.add("aa");
+        beroepen.add("bb");
+        beroepen.add("cc");
+        User ingelogdeUser = new User("gert","gertje","gertje@gmail.com","gert","m","25-09-2001","admin",beroepen);
+        products.add(new Product("product1",2.10,new Category("gaming")));
+        products.add(new Product("product2",2.10,new Category("gaming")));
+        Receipt receipt = new Receipt(ingelogdeUser,products);
+        System.out.println(receipt.getReceipt());
     }
 }
