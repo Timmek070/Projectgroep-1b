e3a1e9959ffe2e7ea3501296a9be37d13288ed5d
Jordy <20169892@student.hhs.nl>
2021-06-07 16:55:50 +0200

    Producten tonen.

--- src/main/java/controller/ShopController.java
+++ src/main/java/controller/ShopController.java
@@ -1,16 +1,31 @@
 package controller;
 
+import Webshop.Category;
+import Webshop.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ShopController {
 
     @FXML
     private AnchorPane rootPane;
+
+    public ShopController() {
+        new Table();
+    }
 
     public void backToHome(MouseEvent mouseEvent) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/HomeScreen.fxml"));
@@ -20,5 +35,39 @@ public class ShopController {
     public void toShop(MouseEvent mouseEvent) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/ShoppingCartScreen.fxml"));
         rootPane.getChildren().setAll(pane);
+    }
+
+    public class Table implements Initializable {
+
+        @FXML
+        private TableView<Product> table;
+
+        @FXML
+        private TableColumn<Product, String> name;
+
+        @FXML
+        private TableColumn<Product, Double> price;
+
+        @FXML
+        private TableColumn<Product, Category> category;
+
+        ObservableList<Category> categorylist = FXCollections.observableArrayList(
+                new Category("woodworking"),
+                new Category("Barend")
+        );
+
+        ObservableList<Product> list = FXCollections.observableArrayList(
+                new Product("Kaas", 2.00, categorylist.get(0)),
+                new Product("Hout", 10.00, categorylist.get(1))
+        );
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            name.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
+            price.setCellValueFactory(new PropertyValueFactory<Product, Double>("price"));
+            category.setCellValueFactory(new PropertyValueFactory<Product, Category>("category"));
+
+            table.setItems(list);
+        }
     }
 }
