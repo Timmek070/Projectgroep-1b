2e976dde8a4935e0085c9538d363511ec036faba
Jordy <72872935+Mytoplaner@users.noreply.github.com>
2021-06-08 15:36:58 +0200

    Update ShopController.java

--- src/main/java/controller/ShopController.java
+++ src/main/java/controller/ShopController.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
@@ -18,14 +19,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ShopController {
+public class ShopController implements Initializable {
 
     @FXML
     private AnchorPane rootPane;
 
-    public ShopController() {
-        new Table();
-    }
+    @FXML
+    private TableView<Product> table;
+
+    @FXML
+    private TableColumn<Product, String> name;
+
+    @FXML
+    private TableColumn<Product, Double> price;
+
+    @FXML
+    private TableColumn<Product, Category> category;
+
+    @FXML
+    private TableColumn<Product, Button> button;
 
     public void backToHome(MouseEvent mouseEvent) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/HomeScreen.fxml"));
@@ -37,37 +49,23 @@ public class ShopController {
         rootPane.getChildren().setAll(pane);
     }
 
-    public class Table implements Initializable {
+    ObservableList<Category> categorylist = FXCollections.observableArrayList(
+            new Category("woodworking"),
+            new Category("Barend")
+    );
 
-        @FXML
-        private TableView<Product> table;
+    ObservableList<Product> list = FXCollections.observableArrayList(
+            new Product("Kaas", 2.00, categorylist.get(0), new Button("voeg toe")),
+            new Product("Barend", 10.00, categorylist.get(1), new Button("voeg toe"))
+    );
 
-        @FXML
-        private TableColumn<Product, String> name;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        name.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
+        price.setCellValueFactory(new PropertyValueFactory<Product, Double>("price"));
+        category.setCellValueFactory(new PropertyValueFactory<Product, Category>("category"));
+        button.setCellValueFactory(new PropertyValueFactory<Product, Button>("button"));
 
-        @FXML
-        private TableColumn<Product, Double> price;
-
-        @FXML
-        private TableColumn<Product, Category> category;
-
-        ObservableList<Category> categorylist = FXCollections.observableArrayList(
-                new Category("woodworking"),
-                new Category("Barend")
-        );
-
-        ObservableList<Product> list = FXCollections.observableArrayList(
-                new Product("Kaas", 2.00, categorylist.get(0)),
-                new Product("Hout", 10.00, categorylist.get(1))
-        );
-
-        @Override
-        public void initialize(URL url, ResourceBundle resourceBundle) {
-            name.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
-            price.setCellValueFactory(new PropertyValueFactory<Product, Double>("price"));
-            category.setCellValueFactory(new PropertyValueFactory<Product, Category>("category"));
-
-            table.setItems(list);
-        }
+        table.setItems(list);
     }
 }
