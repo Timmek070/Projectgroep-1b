d329070de7da804c9031f1ca7569a9223dad7a23
Toon Langendam <t.langendam@gmail.com>
2021-06-09 13:20:48 +0200

    registrereh = done

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -1,18 +1,15 @@
 package controller;
 
 import Account.User;
-import Database.Database;
 import Login.Login;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 import javafx.scene.control.TextField;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class RegisterController{
     @FXML
@@ -33,6 +30,8 @@ public class RegisterController{
     TextField birthdateField;
     @FXML
     TextField roleField;
+    @FXML
+    Text foutmeldingText;
 
     public void backToWelcome(MouseEvent mouseEvent) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/WelcomeScreen.fxml"));
@@ -40,22 +39,23 @@ public class RegisterController{
     }
 
     public void CreateUser(MouseEvent mouseEvent) throws IOException{
-        User user = new User(
-                usernameField.getCharacters().toString(),
-                passwordField.getCharacters().toString(),
-                emailField.getCharacters().toString(),
-                nameField.getCharacters().toString(),
-                sexField.getCharacters().toString(),
-                birthdateField.getCharacters().toString(),
-                roleField.getCharacters().toString()
-        );
+        if(passwordField.getCharacters().toString().equals(confPasswordField.getCharacters().toString())){
+            User user = new User(
+                    usernameField.getCharacters().toString(),
+                    passwordField.getCharacters().toString(),
+                    emailField.getCharacters().toString(),
+                    nameField.getCharacters().toString(),
+                    sexField.getCharacters().toString(),
+                    birthdateField.getCharacters().toString(),
+                    roleField.getCharacters().toString()
+            );
 
-        System.out.println(user);
-        System.out.println(usernameField.getCharacters().toString());
+            Login.getInstance().addUserDatabase(user);
 
-        Login.getInstance().addUserDatabase(user);
-
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
-        this.rootPane.getChildren().setAll(pane);
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
+            this.rootPane.getChildren().setAll(pane);
+        } else {
+            foutmeldingText.setText("wachtwoord komt niet overeen!");
+        }
     }
 }
